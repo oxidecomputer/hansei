71f799dc991cf9497c5e293cd84b2e1cb129209c
Will Chandler <will@oxidecomputer.com>
2025-11-09 20:18:53 -0500

    Handle many types for count

--- dwarf2ctf/src/main.rs
+++ dwarf2ctf/src/main.rs
@@ -1034,8 +1034,6 @@ impl<'a, R: Reader<Offset = usize>> DwarfParser<'a, R> {
     ) -> Result<(Option<u32>, Option<UnitOffset>)> {
         let mut count = None;
         let mut index_type_offset = None;
-        let mut lower_bound = None;
-        let mut upper_bound = None;
 
         let mut attrs = entry.attrs();
         while let Some(attr) = attrs.next()? {
@@ -1045,40 +1043,20 @@ impl<'a, R: Reader<Offset = usize>> DwarfParser<'a, R> {
                         index_type_offset = Some(off);
                     }
                 }
-                gimli::DW_AT_count => {
-                    if let AttributeValue::Udata(val) = attr.value() {
-                        count = Some(val as u32);
-                    }
-                }
-                gimli::DW_AT_lower_bound => match attr.value() {
-                    AttributeValue::Sdata(val) => lower_bound = Some(val as u32),
-                    AttributeValue::Udata(val) => lower_bound = Some(val as u32),
-                    AttributeValue::Data1(val) => lower_bound = Some(val as u32),
-                    AttributeValue::Data2(val) => lower_bound = Some(val as u32),
-                    AttributeValue::Data4(val) => lower_bound = Some(val),
-                    AttributeValue::Data8(val) => lower_bound = Some(val as u32),
-                    _ => {}
-                },
-                gimli::DW_AT_upper_bound => match attr.value() {
-                    AttributeValue::Sdata(val) => upper_bound = Some(val as u32),
-                    AttributeValue::Udata(val) => upper_bound = Some(val as u32),
-                    AttributeValue::Data1(val) => upper_bound = Some(val as u32),
-                    AttributeValue::Data2(val) => upper_bound = Some(val as u32),
-                    AttributeValue::Data4(val) => upper_bound = Some(val),
-                    AttributeValue::Data8(val) => upper_bound = Some(val as u32),
+                gimli::DW_AT_count => match attr.value() {
+                    AttributeValue::Sdata(val) => count = Some(val as u32),
+                    AttributeValue::Udata(val) => count = Some(val as u32),
+                    AttributeValue::Data1(val) => count = Some(val as u32),
+                    AttributeValue::Data2(val) => count = Some(val as u32),
+                    AttributeValue::Data4(val) => count = Some(val),
+                    AttributeValue::Data8(val) => count = Some(val as u32),
                     _ => {}
                 },
                 _ => {}
             }
         }
 
-        let nelems = match (count, lower_bound, upper_bound) {
-            (Some(c), _, _) => Some(c),
-            (None, Some(l), Some(u)) => Some(u - l + 1),
-            _ => None,
-        };
-
-        Ok((nelems, index_type_offset))
+        Ok((count, index_type_offset))
     }
 
     fn parse_struct_type(
