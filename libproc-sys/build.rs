e72097da7f747f2cdc73af93302d1ad0514b6d59
Will Chandler <will@oxidecomputer.com>
2026-02-08 14:37:36 +0000

    Cleanup cruft from libproc-sys build script

--- libproc-sys/build.rs
+++ libproc-sys/build.rs
@@ -11,7 +11,6 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("/usr/include/libproc.h")
-        //.header("/usr/include/procfs.h")
         .generate_comments(true)
         .derive_debug(false)
         .wrap_unsafe_ops(true) // https://github.com/rust-lang/rust-bindgen/issues/3147
@@ -24,15 +23,7 @@ fn main() {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("unable to write libproc bindings");
 
-    //fs::copy(out_path.join("bindings.rs"), "src/lib.rs").unwrap();
-
     println!("cargo:rustc-link-lib=proc");
     println!("cargo:rerun-if-changed=/usr/include/libproc.h");
     println!("cargo:out_dir={}", env::var("OUT_DIR").unwrap());
-
-    // Allow dependent crates to locate the sources and output directory of this crate.
-    // println!(
-    //     "cargo:cargo_manifest_dir={}",
-    //     env::var("CARGO_MANIFEST_DIR").unwrap()
-    // );
 }
